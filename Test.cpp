d529d26fcea8de1c117de346f67352a7facef44d
kfir <kggold4@gmail.com>
2021-04-12 22:07:50 +0300

    add read_units to the Test.cpp file

--- Test.cpp
+++ Test.cpp
@@ -1,68 +1,183 @@
+/**
+ * 
+ * Assignment #3
+ * COURSE:   Systems Programming 2 Ariel University
+ * AUTHORS:  Kfir Goldfarb
+ * https://www.github.com/kggold4
+ * YEAR: 2021
+ * 
+ * */
+
+// includes
+#include <iostream>
+#include <fstream>
 #include "doctest.h"
 #include "NumberWithUnits.hpp"
 
+// namespaces
 using namespace std;
 using namespace ariel;
 
+// units text file
+ifstream units_file{"units.txt"};
+
 /** km and m tests **/
-TEST_CASE("{km, m}: Comparison operators { ==, != }") {}
-TEST_CASE("{km, m}: Comparison operators { <=, < }") {}
-TEST_CASE("{km, m}: Comparison operators { >=, > }") {}
-TEST_CASE("{km, m}: arithmetic operators { ++, -- }") {}
-TEST_CASE("{km, m}: arithmetic operators { +, += ") {}
-TEST_CASE("{km, m}: arithmetic operators { -, -= }") {}
-TEST_CASE("{km, m}: arithmetic operators { *, *= }") {}
+TEST_CASE("{km, m}: Comparison operators { ==, != }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{km, m}: Comparison operators { <=, < }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{km, m}: Comparison operators { >=, > }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{km, m}: arithmetic operators { ++, -- }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{km, m}: arithmetic operators { +, += ") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{km, m}: arithmetic operators { -, -= }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{km, m}: arithmetic operators { *, *= }") {
+    NumberWithUnits::read_units(units_file);
+}
 
 /** m and cm tests **/
-TEST_CASE("{m, cm}: Comparison operators { ==, != }") {}
-TEST_CASE("{m, cm}: Comparison operators { <=, < }") {}
-TEST_CASE("{m, cm}: Comparison operators { >=, > }") {}
-TEST_CASE("{m, cm}: arithmetic operators { ++, -- }") {}
-TEST_CASE("{m, cm}: arithmetic operators { +, += ") {}
-TEST_CASE("{m, cm}: arithmetic operators { -, -= }") {}
-TEST_CASE("{m, cm}: arithmetic operators { *, *= }") {}
+TEST_CASE("{m, cm}: Comparison operators { ==, != }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{m, cm}: Comparison operators { <=, < }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{m, cm}: Comparison operators { >=, > }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{m, cm}: arithmetic operators { ++, -- }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{m, cm}: arithmetic operators { +, += ") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{m, cm}: arithmetic operators { -, -= }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{m, cm}: arithmetic operators { *, *= }") {
+    NumberWithUnits::read_units(units_file);
+}
 
 /** ton and kg tests **/
-TEST_CASE("{ton, kg}: Comparison operators { ==, != }") {}
-TEST_CASE("{ton, kg}: Comparison operators { <=, < }") {}
-TEST_CASE("{ton, kg}: Comparison operators { >=, > }") {}
-TEST_CASE("{ton, kg}: arithmetic operators { ++, -- }") {}
-TEST_CASE("{ton, kg}: arithmetic operators { +, += ") {}
-TEST_CASE("{ton, kg}: arithmetic operators { -, -= }") {}
-TEST_CASE("{ton, kg}: arithmetic operators { *, *= }") {}
+TEST_CASE("{ton, kg}: Comparison operators { ==, != }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{ton, kg}: Comparison operators { <=, < }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{ton, kg}: Comparison operators { >=, > }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{ton, kg}: arithmetic operators { ++, -- }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{ton, kg}: arithmetic operators { +, += ") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{ton, kg}: arithmetic operators { -, -= }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{ton, kg}: arithmetic operators { *, *= }") {
+    NumberWithUnits::read_units(units_file);
+}
 
 /** kg and g tests **/
-TEST_CASE("{kg, g}: Comparison operators { ==, != }") {}
-TEST_CASE("{kg, g}: Comparison operators { <=, < }") {}
-TEST_CASE("{kg, g}: Comparison operators { >=, > }") {}
-TEST_CASE("{kg, g}: arithmetic operators { ++, -- }") {}
-TEST_CASE("{kg, g}: arithmetic operators { +, += ") {}
-TEST_CASE("{kg, g}: arithmetic operators { -, -= }") {}
-TEST_CASE("{kg, g}: arithmetic operators { *, *= }") {}
+TEST_CASE("{kg, g}: Comparison operators { ==, != }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{kg, g}: Comparison operators { <=, < }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{kg, g}: Comparison operators { >=, > }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{kg, g}: arithmetic operators { ++, -- }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{kg, g}: arithmetic operators { +, += ") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{kg, g}: arithmetic operators { -, -= }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{kg, g}: arithmetic operators { *, *= }") {
+    NumberWithUnits::read_units(units_file);
+}
 
 /** hour and min tests **/
-TEST_CASE("{hour, min}: Comparison operators { ==, != }") {}
-TEST_CASE("{hour, min}: Comparison operators { <=, < }") {}
-TEST_CASE("{hour, min}: Comparison operators { >=, > }") {}
-TEST_CASE("{hour, min}: arithmetic operators { ++, -- }") {}
-TEST_CASE("{hour, min}: arithmetic operators { +, += ") {}
-TEST_CASE("{hour, min}: arithmetic operators { -, -= }") {}
-TEST_CASE("{hour, min}: arithmetic operators { *, *= }") {}
+TEST_CASE("{hour, min}: Comparison operators { ==, != }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{hour, min}: Comparison operators { <=, < }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{hour, min}: Comparison operators { >=, > }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{hour, min}: arithmetic operators { ++, -- }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{hour, min}: arithmetic operators { +, += ") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{hour, min}: arithmetic operators { -, -= }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{hour, min}: arithmetic operators { *, *= }") {
+    NumberWithUnits::read_units(units_file);
+}
 
 /** min and sec tests **/
-TEST_CASE("{min, sec}: Comparison operators { ==, != }") {}
-TEST_CASE("{min, sec}: Comparison operators { <=, < }") {}
-TEST_CASE("{min, sec}: Comparison operators { >=, > }") {}
-TEST_CASE("{min, sec}: arithmetic operators { ++, -- }") {}
-TEST_CASE("{min, sec}: arithmetic operators { +, += ") {}
-TEST_CASE("{min, sec}: arithmetic operators { -, -= }") {}
-TEST_CASE("{min, sec}: arithmetic operators { *, *= }") {}
+TEST_CASE("{min, sec}: Comparison operators { ==, != }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{min, sec}: Comparison operators { <=, < }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{min, sec}: Comparison operators { >=, > }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{min, sec}: arithmetic operators { ++, -- }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{min, sec}: arithmetic operators { +, += ") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{min, sec}: arithmetic operators { -, -= }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{min, sec}: arithmetic operators { *, *= }") {
+    NumberWithUnits::read_units(units_file);
+}
 
 /** USD and ILS tests **/
-TEST_CASE("{USD, ILS}: Comparison operators { ==, != }") {}
-TEST_CASE("{USD, ILS}: Comparison operators { <=, < }") {}
-TEST_CASE("{USD, ILS}: Comparison operators { >=, > }") {}
-TEST_CASE("{USD, ILS}: arithmetic operators { ++, -- }") {}
-TEST_CASE("{USD, ILS}: arithmetic operators { +, += ") {}
-TEST_CASE("{USD, ILS}: arithmetic operators { -, -= }") {}
-TEST_CASE("{USD, ILS}: arithmetic operators { *, *= }") {}
+TEST_CASE("{USD, ILS}: Comparison operators { ==, != }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{USD, ILS}: Comparison operators { <=, < }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{USD, ILS}: Comparison operators { >=, > }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{USD, ILS}: arithmetic operators { ++, -- }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{USD, ILS}: arithmetic operators { +, += ") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{USD, ILS}: arithmetic operators { -, -= }") {
+    NumberWithUnits::read_units(units_file);
+}
+TEST_CASE("{USD, ILS}: arithmetic operators { *, *= }") {
+    NumberWithUnits::read_units(units_file);
+}
