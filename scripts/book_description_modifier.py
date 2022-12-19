86778c8be2c41278d932d95fb34849e1e5673098
Kfir Goldfarb <kggold4@gmail.com>
2022-12-19 21:04:12 +0200

    add book 1120 and add book description modifier script

---
+++ scripts/book_description_modifier.py
@@ -0,0 +1,7 @@
+def description_modifier(description: str) -> str:
+    return description.replace('\n', '<br>').strip()
+
+
+if __name__ == '__main__':
+    desc = """"""
+    print(description_modifier(description=desc))
