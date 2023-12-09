84cf871dddafb89dfe8ebcee19fee7b3d033a1fc
kggold4 <kggold4@gmail.com>
2023-12-10 00:24:48 +0200

    new mananger

--- controller.js
+++ controller.js
@@ -1,23 +1,22 @@
 const axios = require("axios");
-const storage = require("mini-db");
 require("dotenv").config();
 
-const db = storage("./storage/books.json");
-const rivhit_url = "https://icredit.rivhit.co.il/API/PaymentPageRequest.svc/";
-
+const rivhit_url = "https://icredit.rivhit.co.il/API/PaymentPageRequest.svc";
 let redirectURL = "https://www.scarlet-publishing.com/pages/order_confirm.php";
-// if (process.env.NODE_ENV === "production") {
-//   redirectURL = "https://scarlet-pub-server.herokuapp.com/redirectURL";
-// } else {
-//   redirectURL = "http://localhost:5000/redirectURL";
-// }
 
 const purchase = (req, res) => {
+  // console.log(`Items: ${req.body.Items}`);
+  // console.log(`details: ${req.body.details}`);
+  console.log("Items: (json) ", JSON.stringify(req.body.Items));
+  console.log("\n\n");
+  console.log("details: (json) ", JSON.stringify(req.body.details));
+  console.log("\n\n");
   axios
     .post(
       `${rivhit_url}/GetUrl`,
       {
-        GroupPrivateToken: process.env.GROUP_PRIVATE_TOKEN,
+        // GroupPrivateToken: process.env.GROUP_PRIVATE_TOKEN,
+        GroupPrivateToken: "39f6f952-043d-4452-abe7-e161c8d4245e",
         RedirectURL: redirectURL,
         ExemptVAT: false,
         MaxPayments: 12,
@@ -26,69 +25,16 @@ const purchase = (req, res) => {
       }
     )
     .then((response) => {
-      placeOrder(response.data);
-      res.send(response.data);
+      const responseUrl = response.data.URL;
+      console.log(`responseUrl: ${responseUrl}`);
+      res.status(200).json({ iframe_url: responseUrl });
     })
     .catch((error) => {
+      console.error(`error: ${error}`);
       res.send(error);
     });
-};
-
-const placeOrder = (order) => {
-  db.insert("orders", {
-    PrivateSaleToken: order.PrivateSaleToken,
-    PublicSaleToken: order.PublicSaleToken,
-  });
-};
-
-const saleDetails = (req, res) => {
-  const saleToken = req.query.PublicSaleToken || req.body.PublicSaleToken;
-  const order = db.select("orders").find((o) => o.PublicSaleToken == saleToken);
-  if (!order) {
-    res.status(400).send("Order not found");
-    return;
-  }
-  axios
-    .post(
-      `${rivhit_url}/SaleDetails`,
-      {
-        "SalePrivateToken": order.PrivateSaleToken,
-      }
-    )
-    .then((response) => {
-      res.send(response.data);
-    })
-    .catch((error) => {
-      res.send(error);
-    });
-};
-
-const getBooks = (req, res) => {
-  res.send(db.select("books"));
-};
-
-const addBook = (req, res) => {
-  const book = req.body;
-  const stock = db.select("books");
-  const stockBook = stock.find((b) => b.CatalogNumber == book.CatalogNumber);
-  if (stockBook) {
-    res
-      .status(400)
-      .send(`Book with CatalogNumber ${book.CatalogNumber} already exists`);
-    return;
-  } else if (!book.CatalogNumber || !book.Description || !book.UnitPrice) {
-    res
-      .status(400)
-      .send("Book must have an CatalogNumber, Description and UnitPrice");
-  } else {
-    db.insert("books", book);
-    res.send(db.select("books"));
-  }
 };
 
 module.exports = {
-  purchase,
-  saleDetails,
-  getBooks,
-  addBook,
+  purchase
 };
--- middlewares.js
+++ middlewares.js
@@ -1,54 +1,50 @@
-const storage = require("mini-db");
+const axios = require('axios');
+const SERVER_ADDRESS = "https://manager-web-server.onrender.com";
 
-const db = storage("./storage/books.json");
-
-const purchase_middleware = (req, res, next) => {
+const purchase_middleware = async (req, res, next) => {
     console.log("purchase_middleware");
     let badRequest = false;
     let { Items, details } = req.body;
-    const stock = db.select("books");
-    if (!Items || Items.length === 0) {
-      res.status(400).send("No books to purchase");
-      return;
+
+    try {
+        const params = {
+          no_images: true
+        };
+        const response = await axios.get(`${SERVER_ADDRESS}/get_books`, { params });
+        const apiBooks = response.data.books;
+
+        if (!Items || Items.length === 0) {
+          console.warn('No books to purchase');
+          res.status(400).send('No books to purchase');
+          return;
+        }
+
+        req.body.Items = Items.map((book) => {
+            const apiBook = apiBooks.find((b) => b.CatalogNumber == book.CatalogNumber);
+            if (!apiBook) {
+              console.warn(`Book with CatalogNumber ${book.CatalogNumber} not found`);
+              res.status(400).send(`Book with CatalogNumber ${book.CatalogNumber} not found`);
+              badRequest = true;
+              return;
+            }
+            return {
+                ...apiBook,
+                Quantity: book.Quantity,
+            };
+        });
+
+        if (badRequest) {
+          console.error("Bad request");
+            return;
+        }
+
+        next();
+    } catch (error) {
+        console.error('Error fetching books from the Flask API', error);
+        res.status(500).send('Internal Server Error');
     }
-    req.body.Items = Items.map((book) => {
-      const stockBook = stock.find((b) => b.CatalogNumber == book.CatalogNumber);
-      if (!stockBook) {
-        res
-          .status(400)
-          .send(`Book with CatalogNumber ${book.CatalogNumber} not found`);
-        badRequest = true;
-        return;
-      }
-      return {
-        ...stockBook,
-        Quantity: book.Quantity,
-      };
-    });
-    if (badRequest) {
-      return;
-    }
-    next();
-}
-
-const saleDetails_middleware = (req, res, next) => {
-    console.log("saleDetails_middleware");
-    next();
-}
-
-const getBooks_middleware = (req, res, next) => {
-    console.log("getBooks_middleware");
-    next();
-}
-
-const addBook_middleware = (req, res, next) => {
-    console.log("addBook_middleware");
-    next();
-}
+};
 
 module.exports = {
     purchase_middleware,
-    saleDetails_middleware,
-    getBooks_middleware,
-    addBook_middleware
-}
+};
