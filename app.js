273eee8e32202f672acba87950caf2e9ab15c88d
kggold4 <kggold4@gmail.com>
2023-12-22 00:11:11 +0200

    update port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const controller = require("./controller");
 const middleware = require("./middlewares");
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 /* Middlewares */
